2bef1c4af3d4c328803349812af61c80dde98afc
autrefouel <Aubry.TREFOUEL@etu.uca.fr>
2021-12-26 18:26:38 +0100

    add existing file

---
+++ README.md
@@ -0,0 +1,16 @@
+# 2CSSF - Frameworks CSS (Bootstrap)
+
+## Installer les dépendences
+```
+yarn install
+```
+
+### Compiler automatiquement lorsqu'un fichier est modifié
+```
+yarn watch
+```
+
+### Compiler pour la production
+```
+yarn build
+```
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'public/dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              // Prefer `dart-sass`
+              implementation: require("sass"),
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
